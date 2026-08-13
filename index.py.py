4c28f00232b9d63003ea038f3f47def4683450d8
asadullahrafiq94-prog <asadullahrafiq94@gmail.com>
2026-08-13 03:17:39 -0700

    Add files via upload

---
+++ index.py.py
@@ -0,0 +1,22 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+
+print=input("please you enter your age:")
+print=input("please enter yor phone number:")
+print=input("please enter  address:")
+print=input("pleae enter your email:")
+print=input("please enter your gender:")
+print=input("please enter your date of birth:")
+print=input("please enter your nationality:")
+print=input("please enter your marital status:")
+a=int(input ("Enter your number x"))
+b=int(input ("Enter your number y"))
+sum = a*b
+print("sum:",sum)
+
+
+name="asad"
+#string opration
+print (ame.upper())
+print(name.lower())
+print(f"{name}")
